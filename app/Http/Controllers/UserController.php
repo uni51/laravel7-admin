8179b64dc56219aad3b9e585a8cb15f1f893a227
Koichi.Shimomura <unieye51@gmail.com>
2021-04-15 16:41:51 +0900

    2-6: REST

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,11 +4,46 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
     public function index()
     {
         return User::all();
+    }
+
+    public function show($id)
+    {
+        return User::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        $user = User::create($request->all());
+
+        return response($user, Response::HTTP_CREATED); // 201
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+
+        $user->update([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return response($user, Response::HTTP_ACCEPTED); // 202
+    }
+
+    public function destroy($id)
+    {
+        User::destroy($id);
+
+        return response(null, Response::HTTP_NO_CONTENT); //204
     }
 }
