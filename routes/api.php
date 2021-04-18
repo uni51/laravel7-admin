61fba012110b7d3e384e99ed393bee2817fafe09
Koichi.Shimomura <unieye51@gmail.com>
2021-04-18 09:44:52 +0900

    2-11: Middleware

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('login', 'AuthController@login');
-Route::apiResource('users', 'UserController');
+
+Route::group(['middleware' => 'auth:api'], function() {
+    Route::apiResource('users', 'UserController');
+});
